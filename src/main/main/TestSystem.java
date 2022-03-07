972f5998705760942e8ef7cc1a4e19fb304feab4
Lucas <somfic@gmail.com>
2022-03-07 23:01:49 +0100

    rip

--- src/main/main/TestSystem.java
+++ src/main/main/TestSystem.java
@@ -11,22 +11,33 @@ public class TestSystem extends System {
 
     List<Entity> entities = new ArrayList<>();
 
+    TileSet cubes = new TileSet("256x256 Cubes.png", 256, 256);
+    TileSet tiles = new TileSet("256x192 Tiles.png", 256, 192);
+    TileSet trees = new TileSet("256x512 Trees.png", 256, 512);
+
+    OpenSimplexNoise noise = new OpenSimplexNoise(java.lang.System.currentTimeMillis());
+    OpenSimplexNoise treeNoise = new OpenSimplexNoise(java.lang.System.currentTimeMillis() + 1);
+
+    double scale = 0.03;
+    int amount = 300;
+    private Entity camera;
+
     @Override
     public void onStart() {
-
-        TileSet cubes = new TileSet("256x256 Cubes.png", 256, 256);
-        TileSet tiles = new TileSet("256x192 Tiles.png", 256, 192);
-        TileSet trees = new TileSet("256x512 Trees.png", 256, 512);
-
-        OpenSimplexNoise noise = new OpenSimplexNoise(java.lang.System.currentTimeMillis());
-        OpenSimplexNoise treeNoise = new OpenSimplexNoise(java.lang.System.currentTimeMillis() + 1);
-
-        double scale = 0.07;
-        int amount = 100;
-
+        camera = new Entity("Camera");
+        camera.addComponent(new TransformComponent());
+        camera.addComponent(new CameraComponent().setZoom(20));
+        addEntity(camera);
 
         for (int x = -amount; x <= amount; x++) {
             for (int y = -amount; y <= amount; y++) {
+                Entity entity = new Entity("Tile test");
+                entity.addComponent(new TransformComponent().setPosition(x, y));
+                entity.addComponent(new TileComponent());
+
+                TransformComponent transform = entity.getComponent(TransformComponent.class);
+                TileComponent tile = entity.getComponent(TileComponent.class);
+
                 int tileX = 0;
                 int tileY = 0;
 
@@ -38,58 +49,48 @@ public class TestSystem extends System {
                     tileSet = tiles;
                     tileX = 2;
                     tileY = 1;
-                }
-
-                else if (height < 0) {
+                } else if (height < 0) {
                     // Shallow water
                     tileSet = tiles;
                     tileX = 0;
                     tileY = 1;
-                }
-
-                else if (height < 0.2) {
+                } else if (height < 0.2) {
                     // Shore
                     tileSet = tiles;
-                    tileX = (int)Math.round(Math.random() * 2f + 2);
+                    tileX = (int) Math.round(height * 2f + 2);
                     tileY = 2;
-                }
-
-                else if (height < 0.3) {
+                } else if (height < 0.3) {
                     // Sand
                     tileSet = tiles;
-                    tileX = (int)Math.round(Math.random() * 2f + 2);
+                    tileX = (int) Math.round(height * 2f + 2);
                     tileY = 0;
-                }
-
-                else if (height < 0.6) {
+                } else if (height < 0.6) {
                     // Grass
                     tileSet = cubes;
-                    tileX = (int)Math.round(Math.random()* 2f + 2);
+                    tileX = (int) Math.round(height * 2f + 2);
                     tileY = 0;
-                }
-
-                else {
+                } else {
                     // Mountain
                     tileSet = cubes;
-                    tileX = (int)Math.round(Math.random() * 9f);
+                    tileX = (int) Math.round(height * 9f);
                     tileY = 0;
-
-                    if(treeNoise.eval(x * scale, y * scale) > 0.2 && Math.random() > 0.5) {
-                       // Tree
-                       Entity tree = new Entity("Tree");
-                       tree.addComponent(new TransformComponent().setPosition(x, y, -1));
-                       tree.addComponent(new TileComponent(trees).setXY(3, 0));
-                       addEntity(tree);
-                    }
                 }
 
-                Entity tile = new Entity("Tile test");
-                tile.addComponent(new TransformComponent().setPosition(x, y));
-                tile.addComponent(new TileComponent(tileSet).setXY(tileX, tileY));
-
-                addEntity(tile);
-                entities.add(tile);
+                tile.setTileSet(tileSet).setXY(tileX, tileY);
+                addEntity(entity);
+                entities.add(entity);
             }
         }
+
+    }
+
+    double time;
+
+    @Override
+    public void onFixedUpdate(double dt) {
+        time += dt;
+
+        TransformComponent cameraTransform = camera.getComponent(TransformComponent.class);
+        cameraTransform.setPositionX(time * 50);
     }
 }
